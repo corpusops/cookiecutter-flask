1203eb206cdd96cd797c585c0467acf193d567e6
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-04-30 00:42:39 +0200

    finish 12factors compatible module

---
+++ {{cookiecutter.out_dir}}/src/{{cookiecutter.flask_project_name}}/config/instances/prod.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from ..base import *  # noqa
+
+DEPLOY_ENV = 'prod'
+
+try:
+    from ..local import *  # noqa
+except ImportError:
+    pass
+# vim:set et sts=4 ts=4 tw=80:
